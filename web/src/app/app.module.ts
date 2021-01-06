39277406e03e774c9388a03f0c93110c1a940d23
Alexander Staeding <alexanderstaeding@gmail.com>
2021-01-06 16:54:57 +0100

    Fix routing

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,14 +1,14 @@
 import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
+import {AppRoutingModule} from "./app-routing.module";
 import {BrowserModule} from '@angular/platform-browser';
-import {RouterModule} from '@angular/router';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    RouterModule,
     BrowserAnimationsModule
   ],
   bootstrap: [AppComponent]
