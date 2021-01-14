2fa477ab3428839314b1142661388c7f8695c5b5
sg28nyli <simon.giegerich@stud.tu-darmstadt.de>
2021-01-14 20:38:11 +0100

    First draft of the evaluation scheme
    Added routing and translation
    
    Added features to Evaluation Scheme
    
    some stuff
    
    Connected evaluation-scheme with detail component
    
    detail-updates
    
    detail-updates2
    
    removed departmentId
    
    finished evaluation scheme

--- web/src/app/evaluation-scheme/evaluation-scheme.component.ts
+++ web/src/app/evaluation-scheme/evaluation-scheme.component.ts
@@ -1,16 +1,54 @@
-import {Component, OnInit} from "@angular/core"
+import {Component, OnInit} from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import {EvaluationSchemeDetailComponent} from "./evaluation-scheme-detail/evaluation-scheme-detail.component";
+import {EvaluationSchemePreview, EvaluationSchemeService} from "../service/evaluation-scheme.service";
+import {from, Observable} from "rxjs";
 
 @Component({
-  selector: "app-evaluation-scheme",
-  templateUrl: "./evaluation-scheme.component.html",
-  styleUrls: ["./evaluation-scheme.component.scss"]
+  selector: 'app-evaluation-scheme',
+  templateUrl: './evaluation-scheme.component.html',
+  styleUrls: ['./evaluation-scheme.component.scss']
 })
-export class EvaluationSchemeComponent implements OnInit {
+export class EvaluationSchemeComponent implements OnInit{
+  columnsScheme = ['name', 'description', 'createdUtc', 'lastUpdatedUtc']
+  data: Observable<EvaluationSchemePreview[]>
 
-  constructor() {
+  constructor(
+    private evaluationSchemeService: EvaluationSchemeService,
+    public dialog: MatDialog,
+  ) {
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.data = from(this.evaluationSchemeService.getAll())
   }
 
+  openDetail(id: string = null) {
+    this.dialog.open(EvaluationSchemeDetailComponent, {
+      width: '100%',
+      data: {
+        id: id
+      }
+    });
+  }
+
+  showDate(evalSchemeId) : string {
+    const date = new Date(parseInt(evalSchemeId.substring(0, 8), 16) * 1000)
+    return date.toLocaleString()
+  }
+
+  calcDate(lastUpdated: Date) : string {
+    return (new Date (new Date(lastUpdated.toString()).getTime())).toLocaleString()
+  }
+
+  descriptionPreview(description) : string{
+    if(description != null) {
+      let preview = description.substring(0, 50)
+      if (preview != description) {
+        preview = preview + " ..."
+      }
+      return preview
+    }
+    return description
+  }
 }
--- web/src/app/evaluation-scheme/evaluation-scheme.module.ts
+++ web/src/app/evaluation-scheme/evaluation-scheme.module.ts
@@ -1,16 +1,41 @@
-import {NgModule} from "@angular/core"
-import {CommonModule} from "@angular/common"
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import {EvaluationSchemeRoutingModule} from "./evaluation-scheme-routing.module"
-import {EvaluationSchemeComponent} from "./evaluation-scheme.component"
-import {EvaluationSchemeDetailComponent} from "./evaluation-scheme-detail/evaluation-scheme-detail.component"
+import {EvaluationSchemeRoutingModule} from './evaluation-scheme-routing.module';
+import {EvaluationSchemeComponent} from './evaluation-scheme.component';
+import {EvaluationSchemeDetailComponent} from './evaluation-scheme-detail/evaluation-scheme-detail.component';
+import {MatIconModule} from "@angular/material/icon";
+import {MatTableModule} from "@angular/material/table";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatButtonModule} from "@angular/material/button";
+import {MatInputModule} from "@angular/material/input";
+import {TranslateModule} from "@ngx-translate/core";
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatTooltipModule} from "@angular/material/tooltip";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatTreeModule} from "@angular/material/tree";
+import {MatCardModule} from "@angular/material/card";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
 
 
 @NgModule({
   declarations: [EvaluationSchemeComponent, EvaluationSchemeDetailComponent],
   imports: [
     CommonModule,
-    EvaluationSchemeRoutingModule
+    EvaluationSchemeRoutingModule,
+    MatIconModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+    TranslateModule,
+    MatDialogModule,
+    MatTooltipModule,
+    FormsModule,
+    MatTreeModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatSnackBarModule,
   ]
 })
 export class EvaluationSchemeModule {
