1fde577304bf7c7c5d426dabe0566aaa37ece23a
Alexander Staeding <alexanderstaeding@gmail.com>
2021-02-23 19:06:06 +0100

    Make tslint happy

--- web/karma.conf.js
+++ web/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ESAThesisTool'),
+      dir: require('path').join(__dirname, './coverage/ESATT'),
       subdir: '.',
       reporters: [
         {type: 'html'},
