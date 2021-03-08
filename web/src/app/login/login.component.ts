f99cb44c189a46c2dedccd85f234bd21124ac0ac
Elisabeth <elisabeth.kirsten@stud.tu-darmstadt.de>
2021-03-08 22:59:59 +0100

    Added LdapAuth (doesn't work yet)

--- web/src/app/login/login.component.ts
+++ web/src/app/login/login.component.ts
@@ -1,40 +1,56 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {Component, OnInit} from "@angular/core"
+import {FormBuilder, FormGroup, Validators} from "@angular/forms"
 import {AuthService} from "../service/auth.service"
+import {ActivatedRoute, Router} from "@angular/router"
+import {first} from "rxjs/operators"
 
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
-  form: FormGroup;
-  private formSubmitAttempt: boolean;
+  form: FormGroup
+  private formSubmitAttempt: boolean
+  returnUrl: string
 
   constructor(
     private fb: FormBuilder,
-    private authService: AuthService
-  ) {}
+    private authService: AuthService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+  }
 
   ngOnInit() {
     this.form = this.fb.group({
-      userName: ['', Validators.required],
-      password: ['', Validators.required]
-    });
+      userName: ["", Validators.required],
+      password: ["", Validators.required]
+    })
+    this.returnUrl = this.route.snapshot.queryParams["returnUrl"] || "/"
   }
 
   isFieldInvalid(field: string) {
     return (
       (!this.form.get(field).valid && this.form.get(field).touched) ||
       (this.form.get(field).untouched && this.formSubmitAttempt)
-    );
+    )
+  }
+
+  get f() {
+    return this.form.controls
   }
 
   onSubmit() {
     if (this.form.valid) {
-      this.authService.login(this.form.value);
+
+      this.authService.login_ldap(this.f.userName.value, this.f.password.value).pipe(first()).subscribe(data => {
+        this.router.navigate([this.returnUrl])
+      }, error => {
+        console.log(error)
+      })
     }
-    this.formSubmitAttempt = true;
+    this.formSubmitAttempt = true
   }
 }
--- web/src/app/service/auth.service.ts
+++ web/src/app/service/auth.service.ts
@@ -1,35 +1,64 @@
-import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { BehaviorSubject } from 'rxjs';
+import {Injectable} from "@angular/core"
+import {Router} from "@angular/router"
+import {BehaviorSubject} from "rxjs"
 import {User} from "./user.service"
+import {HttpClient, HttpHeaders} from "@angular/common/http"
+import {environment} from "../../environments/environment"
+import {map} from "rxjs/operators"
+
 
 @Injectable()
 export class AuthService {
-  private loggedIn: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(this.tokenAvailable());
+  private loggedIn: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(this.tokenAvailable())
 
   get isLoggedIn() {
-    return this.loggedIn.asObservable();
+    return this.loggedIn.asObservable()
   }
 
   constructor(
-    private router: Router
-  ) {}
+    private router: Router,
+    private http: HttpClient
+  ) {
+  }
+
+  get headers(): HttpHeaders {
+    return new HttpHeaders({
+      "Content-Type": "application/json",
+    })
+  }
+
 
   login(user: User) {
-    if (user.userName !== '' && user.password !== '' ) {
-      this.loggedIn.next(true);
-      localStorage.setItem('token', 'true');
-      this.router.navigate(['/dashboard']);
+    if (user.userName !== "" && user.password !== "") {
+      this.loggedIn.next(true)
+      localStorage.setItem("token", "true")
+      this.router.navigate(["/dashboard"])
     }
   }
 
+  login_ldap(username: string, password: string) {
+    return this.http.post<any>(environment.apiUrl + "/login", {username, password}, {headers: this.headers, withCredentials: true}).pipe(map(user => {
+      console.log("HERE")
+        if (user && user.token) {
+          localStorage.setItem("Token", user.token)
+          localStorage.setItem("Timestamp", "" + new Date())
+          this.loggedIn.next(true)
+          this.router.navigate(["/dashboard"])
+          return user
+        }
+        console.log(user)
+
+        return user
+      }))
+  }
+
   logout() {
-    this.loggedIn.next(false);
-    localStorage.removeItem('token');
-    this.router.navigate(['/login']);
+    this.loggedIn.next(false)
+    localStorage.removeItem("token")
+    this.router.navigate(["/login"])
   }
 
   private tokenAvailable(): boolean {
-    return !!localStorage.getItem('token');
+    return !!localStorage.getItem("token")
   }
 }
