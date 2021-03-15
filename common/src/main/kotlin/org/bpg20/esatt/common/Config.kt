2e0ed1a673741c3f385c1646e57020fc025e282d
Alexander Staeding <alexanderstaeding@gmail.com>
2021-03-15 12:25:41 +0100

    Configuration (#16)
    * Initial work on configuration
    
    * Merge these lines
    
    * Move config loading logic to ESATTModule
    
    * forgot to remove these inputs
    
    * oopsi

---
+++ common/src/main/kotlin/org/bpg20/esatt/common/Config.kt
@@ -0,0 +1,11 @@
+package org.bpg20.esatt.common
+
+import org.spongepowered.configurate.objectmapping.ConfigSerializable
+import org.spongepowered.configurate.objectmapping.meta.Comment
+
+@ConfigSerializable
+class Config {
+
+  @Comment("The MongoDB connection string")
+  var mongodbConnection: String? = "mongodb://localhost:27017"
+}
